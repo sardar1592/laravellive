3019c1c325be87a206aa35dc7737c19558e8d3d7
Sardar Nawab Khan <sardarnawabkhan@gmail.com>
2024-02-27 13:12:12 +0500

    more tweaks

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -17,10 +17,10 @@ class EventFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->randomElement(['Laravel', 'Symfony', 'Vue.js', 'React', 'Angular']) . ' ' 
-                    . $this->faker->randomElement(['Meetup', 'Conference', 'Workshop']),
+            'name' => $this->faker->randomElement(['Laravel', 'Symfony', 'Vue.js', 'React', 'Angular', 'Graphql', 'Hasura']) . ' ' 
+                    . $this->faker->randomElement(['Meetup', 'Conference', 'Workshop', 'Conf']),
             'city' => $this->faker->randomElement(['Lahore', 'Karachi', 'Islamabad', 'Faisalabad', 'Multan']),
-            'venue' => $this->faker->randomElement(['Online', 'PC', 'Marriot', 'Arfa Technology Park', 'LUMS']),
+            'venue' => $this->faker->randomElement(['Online', 'PC', 'Marriot', 'Arfa Technology Park', 'LUMS', 'Expo Center', 'Avari']),
             'date' => $this->faker->dateTimeThisMonth(),
             'online' => $this->faker->boolean(),
 
